5f89cf0eb432f8938c188e5c8b331ea06e31c6a4
senthilj94 <senthilj94@users.noreply.github.com>
2020-03-30 22:44:37 +0530

    validators, form builder, value changes

--- src/app/pages/employees/create-employees/create-employees.component.ts
+++ src/app/pages/employees/create-employees/create-employees.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-employees',
@@ -10,22 +10,53 @@ export class CreateEmployeesComponent implements OnInit {
 
   employeeFormGroup: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.employeeFormGroup = new FormGroup({
-      fullName: new FormControl(),
-      email: new FormControl(),
-      skills: new FormGroup({
-        skillName: new FormControl(),
-        experienceInYears: new FormControl(),
-        proficiency: new FormControl()
+    // this.employeeFormGroup = new FormGroup({
+    //   fullName: new FormControl(),
+    //   email: new FormControl(),
+    //   skills: new FormGroup({    // Nested Form Group
+    //     skillName: new FormControl(),
+    //     experienceInYears: new FormControl(),
+    //     proficiency: new FormControl()
+    //   })
+    // });
+
+    this.employeeFormGroup = this.fb.group({
+      fullName: ['', [Validators.required, Validators.minLength(2)]],
+      email: [''],
+      skills: this.fb.group({    // Nested Form Group
+        skillName: [''],
+        experienceInYears: [''],
+        proficiency: ['beginner']
       })
+    });
+
+    this.employeeFormGroup.get('fullName').valueChanges.subscribe(value => {
+      console.log(value);
+    });
+
+    this.employeeFormGroup.valueChanges.subscribe(value => {
+      console.log(value);
     });
   }
 
+  onLoadDataClick() {
+    this.employeeFormGroup.patchValue({ // also set value
+      fullName: 'senthil',
+      email: 'senthil@gmail.com',
+      skills: {
+        skillName: 'fullstack',
+        experienceInYears: '4',
+        proficiency: 'intermediate'
+      }
+    });
+
+  }
+
   onSubmit() {
-    console.log(this.employeeFormGroup.value);
+    console.log(this.employeeFormGroup);
     console.log(this.employeeFormGroup.controls.email.touched);
     console.log(this.employeeFormGroup.get('email').value);
   }
